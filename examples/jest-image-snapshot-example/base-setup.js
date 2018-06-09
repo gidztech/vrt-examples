a6a0ecacb14149829ca532e2d848fc28a90fb548
gidztech <gjpyzer+github@gmail.com>
2018-06-09 13:09:14 +0100

    Make sure to create the tmp dir

--- examples/jest-image-snapshot-example/base-setup.js
+++ examples/jest-image-snapshot-example/base-setup.js
@@ -2,25 +2,35 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const path = require('path');
 const fs = require('fs');
+const { promisify } = require('util');
+const mkdirp = require('mkdirp');
 
-module.exports = (testType) => {
-    return async () => {
-        const app = express();
-        const directory = testType === 'unit' ? 'dist' : 'app';
-        app.use(express.static(path.join(__dirname, '../../', directory)));
-    
-        app.get('/', (req, res) => {
-            res.sendFile('index.html');
-        });
-    
-        global.__SERVER__ = app.listen(3000);
-    
-        const browser = await puppeteer.launch({});
-        global.__PUPPETEER__ = puppeteer;
-        global.__BROWSER__ = browser;
-        fs.writeFileSync(
-            path.join(__dirname, 'tmp', 'puppeteerEndpoint'),
-            browser.wsEndpoint()
-        );
-    } 
+const mkdirIfRequired = async dir => {
+    if (!(await promisify(fs.exists)(dir))) {
+        await mkdirp(dir);
+    }
+    return dir;
+};
+
+module.exports = testType => async () => {
+    const app = express();
+    const directory = testType === 'unit' ? 'dist' : 'app';
+    app.use(express.static(path.join(__dirname, '../../', directory)));
+
+    app.get('/', (req, res) => {
+        res.sendFile('index.html');
+    });
+
+    global.__SERVER__ = app.listen(3000);
+
+    const browser = await puppeteer.launch({});
+    global.__PUPPETEER__ = puppeteer;
+    global.__BROWSER__ = browser;
+
+    await mkdirIfRequired(path);
+
+    fs.writeFileSync(
+        path.join(__dirname, 'tmp', 'puppeteerEndpoint'),
+        browser.wsEndpoint()
+    );
 };
