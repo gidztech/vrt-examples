e0f43b39a24ff78afd197c4845727072428aacdf
gidztech <gjpyzer+github@gmail.com>
2018-07-12 11:57:47 +0100

    #1 Use Fix hanging process

--- examples/jest-image-snapshot-example/teardown.js
+++ examples/jest-image-snapshot-example/teardown.js
@@ -1,3 +1,6 @@
-module.exports = async () => {
+const { teardown: teardownPuppeteer } = require('jest-environment-puppeteer');
+
+module.exports = async function globalTeardown() {
     global.__SERVER__.close();
+    await teardownPuppeteer();
 };
