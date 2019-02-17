a8b85a4cd1c34553351b6944a0df767b34eaea7e
gidztech <gjpyzer+github@gmail.com>
2019-02-17 18:50:57 +0000

    Gsfsd

--- load-nvm.sh
+++ load-nvm.sh
@@ -1,3 +1,3 @@
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# load-nvm.sh
+export NVM_DIR="/opt/circleci/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
